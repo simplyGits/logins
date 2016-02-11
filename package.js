4c1134b5df71cd400481cc69e5d153d2e9a64bfb
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-02-11 20:30:31 +0100

    up

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'simply:logins',
-	version: '1.0.2',
+	version: '1.0.3',
 	summary: 'simple login management',
 	git: 'https://github.com/simplyGits/logins',
 	documentation: 'README.md',
